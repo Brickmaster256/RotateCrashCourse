84425bdfcecc05b10a95b82eedd996d373f5f2d9
Matthew Camacho <mcamacho2056@gmail.com>
2025-02-19 11:39:50 -0700

    added folders for organization and a script to the player to move it

---
+++ RotationkartCrashCourse/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Rigidbody playerRigidbody;
+     void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidbody.AddForce(transform.right * speed);
+        }
+    }
+}
